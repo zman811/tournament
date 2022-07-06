321b62a19b09e10e1eae03500894e9a2f694dfd5
zman811 <sunburnzman@gmail.com>
2022-07-06 13:09:34 -0600

    Basic chanes to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,25 @@ import type { NextPage } from "next";
 import NavbarEle from "./components/Navbar";
 import Head from "next/head";
 import Link from "next/link";
+import { Anchor, Center, Container, Space, Title } from "@mantine/core";
 
 const Home: NextPage = () => {
   // Want to make this a page for log in and showing the name of the app and a description
   return (
     <>
       <NavbarEle />
-      <div>
-        Hello!
-        <Link href="/api/auth/login">Login</Link>
-        <Link href="/api/auth/logout">Logout</Link>
-      </div>
+      <Space h="xl" />
+      <Center style={{ textAlign: "center" }}>
+        <Container>
+          <Title order={2}>Name of the app here,</Title>
+          <Link href="/api/auth/login">
+            <Title order={3}>
+              <Anchor>Login</Anchor>
+            </Title>
+          </Link>
+          {/* Maybe add in a picture here or something to make the page more appeling */}
+        </Container>
+      </Center>
     </>
   );
 };
