943b0263821191e6d104949a79aade8496b484de
zman811 <sunburnzman@gmail.com>
2022-07-11 16:53:54 -0600

    Added in a modal for each tournamet using placeholder data for now

--- pages/homePage.tsx
+++ pages/homePage.tsx
@@ -1,29 +1,56 @@
 import { useUser } from "@auth0/nextjs-auth0";
-import { Card, Center, Container, Stack, Title, Text, Button } from "@mantine/core";
+import {
+  Card,
+  Center,
+  Container,
+  Stack,
+  Title,
+  Text,
+  Button,
+  Modal,
+} from "@mantine/core";
 import type { NextPage } from "next";
+import { useState } from "react";
 
 const HomePage: NextPage = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [current, setCurrent] = useState(-1);
   const { user } = useUser();
   if (!user?.email) {
     return <div>Loading...</div>;
   }
   return (
     <Center>
+      {isOpen && (
+        <Modal
+          overlayOpacity={0.55}
+          overlayBlur={3}
+          opened={true}
+          onClose={() => {setIsOpen(false)}}
+        >
+          Hello, {current}
+          {/* current is a placeholder for the current tournament that will be selected whenever the modal is opened */}
+        </Modal>
+      )}
       <Container>
         <Title order={2}>Hello, {user?.name}</Title>
         <Stack>
-          {/* Will get an array of tournaments and map over them here */}
-          <Card style={{ width: "500px" }} onClick={() => {}}>
-            <Text>This is a tournament</Text>
-            <Text>
-              It will have a picture of the game that it is for and you will be
-              able to click on it
-            </Text>
-            <Center>
-              {/* This button will take you to the page for that tournament, OR open a modal with more info and a link to see the page for it */}
-              <Button>See more info</Button>
-            </Center>
-          </Card>
+          {[...Array(5)].map((_, i) => {
+            // Here is maping over each tournament
+            return (
+              <Card key={i} style={{ width: "500px" }} onClick={() => {}}>
+                <Text>This is a tournament</Text>
+                <Text>
+                  It will have a picture of the game that it is for and you will
+                  be able to click on it
+                </Text>
+                <Center>
+                  {/* This button will take you to the page for that tournament, by opening a modal with more info and a link to see the page for it */}
+                  <Button onClick={() => {setIsOpen(true); setCurrent(i)}}>See more info</Button>
+                </Center>
+              </Card>
+            );
+          })}
         </Stack>
       </Container>
     </Center>
