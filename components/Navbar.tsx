a5267d065cf6471c9c9af817f0998029801502a7
zman811 <sunburnzman@gmail.com>
2022-07-24 16:11:31 -0600

    Changes i forgot to commit

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,17 @@
 import Link from "next/link";
-import { Anchor, Button, Container, Divider, Grid, Menu, Space } from "@mantine/core";
+import {
+  Anchor,
+  Button,
+  Container,
+  Divider,
+  Grid,
+  Menu,
+  Space,
+} from "@mantine/core";
 import { NextLink } from "@mantine/next";
 import { useUser } from "@auth0/nextjs-auth0";
 
 export default function NavbarEle() {
-  // Route to all pages here in the menu
   const { user } = useUser();
   let name = "";
   if (user) {
@@ -13,44 +20,44 @@ export default function NavbarEle() {
 
   return (
     <>
-    <Container fluid>
-      <Grid justify="center" align="flex-start">
-        <Grid.Col span={4}>
-          <Link href="/">
-            <Anchor style={{ fontWeight: "bold" }}>Home</Anchor>
-          </Link>
-        </Grid.Col>
-        <Grid.Col span={5} offset={3}>
-          <Grid>
-            <Grid.Col span={4}></Grid.Col>
-            <Grid.Col span={4}>
-              <Link href="/user/${name}">
-                <Anchor>{name}</Anchor>
-              </Link>
-            </Grid.Col>
-            <Grid.Col span={4}>
-              <Menu control={<Button>Menu</Button>}>
-                <Menu.Item component={NextLink} href="/test">
-                  test
-                </Menu.Item>
-                {user?.email && (
-                  <>
-                  <Menu.Item component={NextLink} href="/homePage">
-                    User Page
+      <Container fluid>
+        <Grid justify="center" align="flex-start">
+          <Grid.Col span={4}>
+            <Link href="/">
+              <Anchor style={{ fontWeight: "bold" }}>Home</Anchor>
+            </Link>
+          </Grid.Col>
+          <Grid.Col span={5} offset={3}>
+            <Grid>
+              <Grid.Col span={4}></Grid.Col>
+              <Grid.Col span={4}>
+                <Link href="/user/${name}">
+                  <Anchor>{name}</Anchor>
+                </Link>
+              </Grid.Col>
+              <Grid.Col span={4}>
+                <Menu control={<Button>Menu</Button>}>
+                  <Menu.Item component={NextLink} href="/test">
+                    test
                   </Menu.Item>
-                  <Menu.Item component={NextLink} href="/api/auth/logout">
-                    logout
-                  </Menu.Item>
-                  </>
-                )}
-              </Menu>
-            </Grid.Col>
-          </Grid>
-        </Grid.Col>
-      </Grid>
-    </Container>
-    <Space h="lg"/>
-    <Divider />
+                  {user?.email && (
+                    <>
+                      <Menu.Item component={NextLink} href="/homePage">
+                        User Page
+                      </Menu.Item>
+                      <Menu.Item component={NextLink} href="/api/auth/logout">
+                        logout
+                      </Menu.Item>
+                    </>
+                  )}
+                </Menu>
+              </Grid.Col>
+            </Grid>
+          </Grid.Col>
+        </Grid>
+      </Container>
+      <Space h="lg" />
+      <Divider />
     </>
   );
 }
