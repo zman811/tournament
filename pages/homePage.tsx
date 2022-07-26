165bda89c100b1c6429922683711ac58ccba9166
zman811 <sunburnzman@gmail.com>
2022-07-26 15:57:29 -0600

    Added in a page for eac tournament and routing to it

--- pages/homePage.tsx
+++ pages/homePage.tsx
@@ -15,6 +15,7 @@ import {
 import type { NextPage } from "next";
 import NavbarEle from "@/components/Navbar";
 import { useState } from "react";
+import Link from "next/link";
 
 const HomePage: NextPage = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -25,55 +26,58 @@ const HomePage: NextPage = () => {
   }
   return (
     <>
-    <NavbarEle />
-    <Space h="xl"/>
-    <Center>
-      {isOpen && (
-        <Modal
-          overlayBlur={2}
-          opened={true}
-          onClose={() => {
-            setIsOpen(false);
-          }}
-        >
-          Hello, {current}
-          {/* current is a placeholder for the current tournament that will be selected whenever the modal is opened */}
-        </Modal>
-      )}
-      <Container>
-        <Title order={2}>Hello, {user?.name}</Title>
-        <Stack>
-          {[...Array(5)].map((_, i) => {
-            // Here is maping over each tournament
-            return (
-              <Card key={i} style={{ width: "500px" }} onClick={() => {}}>
-                <Group position="apart">
-                  <Text size="xl" weight="bold">
-                    This is a tournament
+      <NavbarEle />
+      <Space h="xl" />
+      <Center>
+        {isOpen && (
+          <Modal
+            overlayBlur={2}
+            opened={true}
+            onClose={() => {
+              setIsOpen(false);
+            }}
+          >
+            <Title order={3}>This will have info about it, id={current}</Title>
+            <Link href={`/tournaments/${current}`}>
+              <Button component="a">Go to page</Button>
+            </Link>
+            {/* current is a placeholder for the current tournament that will be selected whenever the modal is opened */}
+          </Modal>
+        )}
+        <Container>
+          <Title order={2}>Hello, {user?.name}</Title>
+          <Stack>
+            {[...Array(5)].map((_, i) => {
+              // Here is maping over each tournament
+              return (
+                <Card key={i} style={{ width: "500px" }} onClick={() => {}}>
+                  <Group position="apart">
+                    <Text size="xl" weight="bold">
+                      This is a tournament
+                    </Text>
+                    <Badge>Game</Badge>
+                  </Group>
+                  <Text>
+                    It will have a picture of the game that it is for and you
+                    will be able to click on it
                   </Text>
-                  <Badge>Game</Badge>
-                </Group>
-                <Text>
-                  It will have a picture of the game that it is for and you will
-                  be able to click on it
-                </Text>
-                <Center>
-                  {/* This button will take you to the page for that tournament, by opening a modal with more info and a link to see the page for it */}
-                  <Button
-                    onClick={() => {
-                      setIsOpen(true);
-                      setCurrent(i);
-                    }}
-                  >
-                    See more info
-                  </Button>
-                </Center>
-              </Card>
-            );
-          })}
-        </Stack>
-      </Container>
-    </Center>
+                  <Center>
+                    {/* This button will take you to the page for that tournament, by opening a modal with more info and a link to see the page for it */}
+                    <Button
+                      onClick={() => {
+                        setIsOpen(true);
+                        setCurrent(i);
+                      }}
+                    >
+                      See more info
+                    </Button>
+                  </Center>
+                </Card>
+              );
+            })}
+          </Stack>
+        </Container>
+      </Center>
     </>
   );
 };
---
+++ pages/tournaments/[id].tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const TournamentPage: NextPage = () => {
+  return (<div>Hello</div>)
+}
+
+export default TournamentPage
